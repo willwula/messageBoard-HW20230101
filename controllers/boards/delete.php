d15ddebe5400ce737890b808cf3b314887069baf
willwula <wfh318@gmail.com>
2023-02-10 10:06:26 +0800

    fix: views

--- controllers/boards/delete.php
+++ controllers/boards/delete.php
@@ -1,7 +1,13 @@
 <?php
 use models\Guestbook;
-authorize(@$_POST["action"]&&$_POST["action"]=="delete");
+authorize($_POST["action"]&&$_POST["action"]=="delete");
+
 $delete = new Guestbook();
+$delete->getAllmessageByNo($_POST['no']);
+//dd($_POST);
+//$name = $delete->result['0']['name'];
+//dd($_SESSION['name']);
+authorize($_POST['name'] == $_SESSION['name']);
 $no = @$_POST['no'];
 $delete->delete($no);
 echo "
--- views/partials/nav.php
+++ views/partials/nav.php
@@ -1,8 +1,8 @@
 <nav>
-<div class="top-right home">
-    <a href="/visit">訪客瀏覽</a>
-    <a href="/">登入</a>
-    <a href="/signup">註冊會員</a>
-</div>
+    <div class="top-right home">
+        <a href="/visit">訪客瀏覽</a>
+        <a href="/">登入</a>
+        <a href="/signup">註冊會員</a>
+    </div>
 </nav>
 
